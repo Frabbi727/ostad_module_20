d6147a8696286dd4c12026d64e2ee14a308b01dc
Rabbi <fazle.rabbi@bracits.com>
2026-01-08 13:00:21 +0600

    assignemnt 20

---
+++ src/main/java/org/ostad/assignment_20/Assignment20Application.java
@@ -0,0 +1,13 @@
+package org.ostad.assignment_20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment20Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assignment20Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/ostad/assignment_20/Assignment20ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ostad.assignment_20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment20ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
